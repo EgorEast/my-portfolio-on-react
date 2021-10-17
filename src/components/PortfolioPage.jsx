fa4a1c2a330b8d3e26715b38622fdbdd9c8bec08
EgorEast <jir.jir35@gmail.com>
2021-10-17 21:41:37 +0300

    added bootstrap to used technologies for 'Portfolio'

--- src/components/PortfolioPage.jsx
+++ src/components/PortfolioPage.jsx
@@ -178,7 +178,7 @@ const landingPages = [
 			content: 'on Figma',
 			href: 'https://www.figma.com/file/nDXdlmr1kCVnWEcnPWtfLg/%D0%97%D0%B0%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5-%D0%BD%D0%B0-%D0%91%D1%83%D1%82%D1%81%D1%82%D1%80%D0%B0%D0%BF?node-id=1%3A2',
 		},
-		usedTechnologies: ['JavaScript', 'CSS'],
+		usedTechnologies: ['Bootstrap', 'JavaScript', 'CSS'],
 		links: [
 			{
 				content: 'On GitHub',
